00c843026209b1ad2e7bd1e14c437f50f60fcc8a
KudusMusah <118946022+KudusMusah@users.noreply.github.com>
2024-07-02 11:51:42 +0000

    Update ternary String.py

--- ternary String.py
+++ ternary String.py
@@ -1,26 +1,20 @@
-from collections import defaultdict
-
 def solve(s):
-    hashmap = defaultdict(int)
-    ans = float("inf")
-    
-    l = 0
-    for r in range(len(s)):
-        hashmap[s[r]] += 1
-        
-        while len(hashmap) == 3 and l < r:
-            ans= min(ans, r-l+1)
-            
-            hashmap[s[l]] -= 1
-            if hashmap[s[l]] == 0:
-                del hashmap[s[l]]
-            l += 1
-             
-    if ans == float("inf"): ans = 0
-    print(ans)            
+    hashmap = {}
+
+    ans = float("Inf")
+
+    for i in range(len(s)):
+        hashmap[s[i]] = i
+
+        if len(hashmap) == 3:
+            curr_len = max(hashmap.values()) - min(hashmap.values()) + 1
+            ans = min(ans, curr_len)
+
+    return ans if ans != float("inf") else 0
+
 
 
 for i in range(int(input())):
     s = input()
     
-    solve(s)
+    print(solve(s))
