42345100cbd55a3f76af75a852cfe7a5eddff750
KudusMusah <118946022+KudusMusah@users.noreply.github.com>
2024-07-02 11:52:50 +0000

    Update boken keyboard.py

--- boken keyboard.py
+++ boken keyboard.py
@@ -1,29 +1,22 @@
 def solve(s):
-    output = set()
+    l, r = 0, 0
 
-    if len(s) == 1:
-        return s[0]
+    ans = set()
 
-    if s[0] != s[1]: output.add(s[0])
-    if s[-1] != s[-2]: output.add(s[-1])
-
-    l, r = 0, 1
     while r < len(s):
-        if s[r] != s[l]:
+        if s[l] == s[r]:
+            r += 1
+        else:
             if (r - l) % 2 != 0:
-                output.add(s[l])
+                ans.add(s[l])
             l = r
 
-        if r == len(s) - 1:
-            if (r - l + 1) % 2 != 0:
-                output.add(s[r])
+    if (r - l) % 2 != 0:
+        ans.add(s[l])
 
-        r += 1
-
-    return "".join(sorted(list(output)))
+    return "".join(sorted(list(ans)))
 
 
 for i in range(int(input())):
     s = input()
-
     print(solve(s))
